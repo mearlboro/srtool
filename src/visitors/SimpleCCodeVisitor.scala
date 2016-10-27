401b3ad4b7ddcfe3f2b3717a73e824a70bea7f90
Sam Coope <sam.j.coope@gmail.com>
2016-10-27 16:02:31 +0100

    All local tests passing.

--- src/visitors/SimpleCCodeVisitor.scala
+++ src/visitors/SimpleCCodeVisitor.scala
@@ -78,45 +78,55 @@ class SimpleCCodeVisitor extends SimpleCBaseVisitor[String]{
 
   override def visitTernExpr(ctx: SimpleCParser.TernExprContext): String = visitChildren(ctx)
 
-  override def visitLorExpr(ctx: SimpleCParser.LorExprContext): String =
-    if(ctx.getChildCount == 1) super.visitLorExpr(ctx)
-    else bracket(visit(ctx.landExpr(0)) + ctx.ops.get(0).getText + visit(ctx.landExpr(1)))
-  
-  override def visitLandExpr(ctx: SimpleCParser.LandExprContext):String =
-    if(ctx.getChildCount == 1) super.visitLandExpr(ctx)
-    else bracket(visit(ctx.borExpr(0)) + ctx.ops.get(0).getText + visit(ctx.borExpr(1)))
+  override def visitLorExpr(ctx: SimpleCParser.LorExprContext): String = {
+    val opsStrs = ctx.ops.map(" " + _.getText() + " ")
+    return weaveString(ctx.landExpr().map(x => visit(x)), opsStrs)
+  }
 
-  override def visitBorExpr(ctx: SimpleCParser.BorExprContext):String =
-    if(ctx.getChildCount == 1) super.visitBorExpr(ctx)
-    else bracket(visit(ctx.bxorExpr(0)) + ctx.ops.get(0).getText + visit(ctx.bxorExpr(1)))
+  override def visitLandExpr(ctx: SimpleCParser.LandExprContext):String = {
+    val opsStrs = ctx.ops.map(" " + _.getText() + " ")
+    return weaveString(ctx.borExpr().map(x => visit(x)), opsStrs)
+  }
 
-  override def visitBxorExpr(ctx: SimpleCParser.BxorExprContext):String =
-    if(ctx.getChildCount == 1) super.visitBxorExpr(ctx)
-    else bracket(visit(ctx.bandExpr(0)) + ctx.ops.get(0).getText + visit(ctx.bandExpr(1)))
+  override def visitBorExpr(ctx: SimpleCParser.BorExprContext):String = {
+    val opsStrs = ctx.ops.map(" " + _.getText() + " ")
+    return weaveString(ctx.bxorExpr().map(x => visit(x)), opsStrs)
+  }
 
-  override def visitBandExpr(ctx: SimpleCParser.BandExprContext):String =
-    if(ctx.getChildCount == 1) super.visitBandExpr(ctx)
-    else bracket(visit(ctx.equalityExpr(0)) + ctx.ops.get(0).getText + visit(ctx.equalityExpr(1)))
+  override def visitBxorExpr(ctx: SimpleCParser.BxorExprContext):String = {
+    val opsStrs = ctx.ops.map(" " + _.getText() + " ")
+    return weaveString(ctx.bandExpr().map(x => visit(x)), opsStrs)
+  }
 
-  override def visitEqualityExpr(ctx: SimpleCParser.EqualityExprContext):String =
-    if(ctx.getChildCount == 1) super.visitEqualityExpr(ctx)
-    else bracket(visit(ctx.relExpr(0)) + ctx.ops.get(0).getText + visit(ctx.relExpr(1)))
+  override def visitBandExpr(ctx: SimpleCParser.BandExprContext):String = {
+    val opsStrs = ctx.ops.map(" " + _.getText() + " ")
+    return weaveString(ctx.equalityExpr().map(x => visit(x)), opsStrs)
+  }
 
-  override def visitRelExpr(ctx: SimpleCParser.RelExprContext):String =
-    if(ctx.getChildCount == 1) super.visitRelExpr(ctx)
-    else bracket(visit(ctx.shiftExpr(0))  + ctx.ops.get(0).getText + visit(ctx.shiftExpr(1)))
+  override def visitEqualityExpr(ctx: SimpleCParser.EqualityExprContext):String = {
+    val opsStrs = ctx.ops.map(" " + _.getText() + " ")
+    return weaveString(ctx.relExpr().map(x => visit(x)), opsStrs)
+  }
 
-  override def visitShiftExpr(ctx: SimpleCParser.ShiftExprContext):String =
-    if(ctx.getChildCount == 1) super.visitShiftExpr(ctx)
-    else bracket(visit(ctx.addExpr(0)) + ctx.ops.get(0).getText + visit(ctx.addExpr(1)))
+  override def visitRelExpr(ctx: SimpleCParser.RelExprContext):String = {
+    val opsStrs = ctx.ops.map(" " + _.getText() + " ")
+    return weaveString(ctx.shiftExpr().map(x => visit(x)), opsStrs)
+  }
 
-  override def visitAddExpr(ctx: SimpleCParser.AddExprContext):String =
-    if(ctx.getChildCount == 1) super.visitAddExpr(ctx)
-    else bracket(visit(ctx.mulExpr(0)) + ctx.ops.get(0).getText + visit(ctx.mulExpr(1)))
+  override def visitShiftExpr(ctx: SimpleCParser.ShiftExprContext):String = {
+    val opsStrs = ctx.ops.map(" " + _.getText() + " ")
+    return weaveString(ctx.addExpr().map(x => visit(x)), opsStrs)
+  }
 
-  override def visitMulExpr(ctx: SimpleCParser.MulExprContext):String =
-    if(ctx.getChildCount == 1) super.visitMulExpr(ctx)
-    else bracket(visit(ctx.unaryExpr(0)) + ctx.ops.get(0).getText + visit(ctx.unaryExpr(1)))
+  override def visitAddExpr(ctx: SimpleCParser.AddExprContext):String = {
+    val opsStrs = ctx.ops.map(" " + _.getText() + " ")
+    return weaveString(ctx.mulExpr().map(x => visit(x)), opsStrs)
+  }
+
+  override def visitMulExpr(ctx: SimpleCParser.MulExprContext):String = {
+    val opsStrs = ctx.ops.map(" " + _.getText() + " ")
+    return weaveString(ctx.unaryExpr().map(x => visit(x)), opsStrs)
+  }
 
   override def visitUnaryExpr(ctx: SimpleCParser.UnaryExprContext):String =
     if(ctx.getChildCount == 1) super.visitUnaryExpr(ctx)
@@ -149,5 +159,13 @@ class SimpleCCodeVisitor extends SimpleCBaseVisitor[String]{
   def visitFormalParams(params: java.util.List[SimpleCParser.FormalParamContext]) =
     params.toList.map(visitFormalParam).mkString(", ")
 
+
+  def weaveString(a: Iterable[String], b: Iterable[String]): String = {
+    if (a.size == 1) {
+      return a.head
+    }
+    val str = a.head + b.head
+    return str + weaveString(a.tail, b.tail)
+  }
 
 }
